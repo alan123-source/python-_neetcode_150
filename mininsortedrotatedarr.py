34c5510ddafaa5f997690849ca639b89499ec3aa
alan123-source <alanpauljohn385@gmail.com>
2025-10-30 09:18:46 +0530

    added rotated sortedarray minumum element

---
+++ mininsortedrotatedarr.py
@@ -0,0 +1,17 @@
+class solution:
+    def findmin(self,nums:list[int])->int:
+        l,r=0,len(nums)-1
+        res=nums[0]
+        while l<r:
+            if nums[l]<nums[r]:
+                res=min(res,nums[l])
+                break
+            m=(l+r)//2
+            res=min(res,nums[m])
+            if nums[m]>=nums[l]:
+                l=m+1
+            else:
+                r=m-1
+        return res
+sol=solution()
+print(sol.findmin([4,5,6,7,8,1,2,3]))
